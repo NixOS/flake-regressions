56d71fede757da9e0f0f63672141a01e220b4ffd
Eelco Dolstra <edolstra@gmail.com>
2024-11-06 15:16:25 +0100

    Merge flake-regressions.sh from the Nix repo into eval-all.sh

--- eval-all.sh
+++ eval-all.sh
@@ -2,15 +2,32 @@
 
 set -eu
 
+SCRIPT_DIR="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)"
+
+cd "$SCRIPT_DIR"
+
+echo "Nix version:"
+nix --version
+
 : ${REGENERATE:=0}
 
 export REGENERATE
 
-for i in tests/*/*/*; do
-    marker="$i/done"
-    if [[ -e $marker ]]; then continue; fi
-    if ! ./eval-flake.sh "$i"; then
-        touch "$i/failed"
+status=0
+
+flakes=$(find tests -mindepth 3 -maxdepth 3 -type d -not -path '*/.*' | sort | head -n${MAX_FLAKES:-1000000})
+
+for flake in $flakes; do
+    marker="$flake/done"
+    if [[ ! -e $marker ]]; then
+        if ! ./eval-flake.sh "$flake"; then
+            touch "$flake/failed"
+        fi
+    fi
+    if [[ -e $flake/failed ]]; then
+        echo "❌ $flake"
+    else
+        echo "✅ $flake"
     fi
     touch "$marker"
 done
