41da0573815571777bfdbdecb979e54079644e9e
Eelco Dolstra <edolstra@gmail.com>
2025-04-25 16:30:47 +0200

    Force store creation

--- eval-all.sh
+++ eval-all.sh
@@ -10,4 +10,6 @@ echo "Nix version: $(nix --version)"
 
 export CACHE_RUNS=1
 
+nix store info
+
 find tests -mindepth 3 -maxdepth 3 -type d -not -path '*/.*' | sort | head -n${MAX_FLAKES:-1000000} | parallel ./eval-flake.sh
