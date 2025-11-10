bf2c9efa27d7cafa747dbabbc86ceedc602e1109
Eelco Dolstra <edolstra@gmail.com>
2025-11-10 16:10:54 +0100

    Use same format as `nix flake show --json` from flake-schemas
    You can set `USE_NIX_FLAKE_SHOW=1` to actually use `nix flake show
    --json` rather than the eval-flake.nix emulation.

--- eval-all.sh
+++ eval-all.sh
@@ -10,6 +10,6 @@ echo "Nix version: $(nix --version)"
 
 export CACHE_RUNS=1
 
-nix store info
+nix store info || true
 
 find tests -mindepth 3 -maxdepth 3 -type d -ipath "tests/${FLAKE_REGRESSION_GLOB:-*}" -not -path '*/.*' | sort | head -n${MAX_FLAKES:-1000000} | parallel ./eval-flake.sh
